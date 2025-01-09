687eac38fd99cafc7db1436633305862d5086d0f
Joel Arraigada <joel.arraigada@globant.com>
2025-01-09 11:05:58 -0300

    appbar

--- new_app/lib/main.dart
+++ new_app/lib/main.dart
@@ -13,7 +13,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Restaurants',
-      debugShowCheckedModeBanner: false,
       theme: ThemeData(useMaterial3: true,
       ),
       home: const Restaurant(
--- new_app/lib/pages/restaurant.dart
+++ new_app/lib/pages/restaurant.dart
@@ -16,29 +16,93 @@ class Restaurant extends StatelessWidget{
     required this.parkName,
     required this.img
     });
-  
+
+
   @override
   Widget build(BuildContext context) {
+    
     final screenSize = MediaQuery.of(context).size;
-
-    return Scaffold(
-      appBar: PreferredSize(
-        preferredSize: Size.fromHeight(screenSize.height*0.4),
-        child: 
-        Column(
-          children: [
-            SizedBox(
-              height: screenSize.height *0.3,
-              child: Image.file(
-                File(this.img),
-                fit:BoxFit.scaleDown),
-                width: double.infinity,),
-            Text(this.name),
-          ],
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: SafeArea(
+          child: Column(
+            children: [
+              SizedBox(
+                height: screenSize.height*0.3,
+                child: Container(
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage('assets/images/sunshine-tree-terrace-01-new.jpg'), 
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  this.name)
+              ),
+              
+            ],
+          ),
         ),
-      ),
-      body: Center(
       ),
     );
   }
+
+
+
+
+  //   @override
+  // Widget build(BuildContext context) {
+  //   final screenSize = MediaQuery.of(context).size;
+  //   return Scaffold(
+  //     appBar: AppBar(
+  //       leading: Padding(
+  //       padding: const EdgeInsets.all(8.0),
+  //       child: Column(
+  //         children: [
+  //            SizedBox(
+  //             height: screenSize.height *0.3,
+  //              child: Image.file(
+  //                File(this.img),
+  //               fit:BoxFit.fitWidth),
+  //               width: double.infinity,),
+  //          Text(this.name),
+  //          ],
+  //        )
+  //       )
+  //     ),
+  //   );
+  // }
+  
+  // @override
+  // Widget build(BuildContext context) {
+  //   final screenSize = MediaQuery.of(context).size;
+
+  //   return Scaffold(
+  //     appBar: PreferredSize(
+  //       preferredSize: Size.fromHeight(screenSize.height*0.4),
+  //       child: 
+  //       Column(
+  //         children: [
+  //           SizedBox(
+  //             height: screenSize.height *0.3,
+  //             child: Image.file(
+  //               File(this.img),
+  //               fit:BoxFit.scaleDown),
+  //               width: double.infinity,),
+  //           Text(this.name),
+  //         ],
+  //       ),
+  //     ),
+  //     body: Center(
+  //     ),
+  //   );
+  // }
+
+
+
 }
